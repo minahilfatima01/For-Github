df79d8ef3100d7247cbdb4bc41c86c951c6fbefc
minahilfatima01 <164044013+minahilfatima01@users.noreply.github.com>
2024-03-29 15:17:26 +0500

    product code

---
+++ pro.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int product(int num1, int num2){
+int multiply=num1*num2;
+cout<<"product";
+return num1*num2;
+}
+int main(){
+	int num1,num2;
+		cout<<"enter the value of num1"<<endl;
+	cin>>num1;
+		cout<<"enter the value of num2;"<<endl;
+
+	cin>>num2;
+
+	cout<<"product of"<<num1<<"and"<<num2<<"is"<<endl;
+cout<<	product(num1, num2);
+	
+
+
+
+
+}
+
